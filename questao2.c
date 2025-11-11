8743b90a299866239bda3c0a05a56061acbd85ba
AnnaS2Cat <annagraduacaouerj@gmail.com>
2025-11-11 14:24:15 -0300

    Update questao2.c

--- questao2.c
+++ questao2.c
@@ -5,4 +5,28 @@ struct Caixa {
     struct Caixa* prox;
 };
 
+void exibe (struct Caixa* caixa){
+    while(caixa!= NULL){
+        printf("%d ", caixa->valor);
+        caixa = caixa->prox;
+    }
+    printf("\n");
+}
+
+int main(void){
+    struct Caixa c1, c2, c3, c4, c5;
+    c1.valor = 1;
+    c2.valor = 3;
+    c3.valor = 9;
+    c4.valor = 7;
+    c5.valor = 5;
+
+    c1.prox = &c2;
+    c2.prox = &c3;
+    c3.prox = &c4;
+    c4.prox = &c5;
+    c5.prox = NULL; 
+
+    void exibe (&c1); //chamo o endereço da primeira caixa para percorrer os nós a partir do primeiro
+}
 
